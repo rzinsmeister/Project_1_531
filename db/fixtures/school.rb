c4937640a986ab02275ee24225c501e4e85d112d
Ruth Zinsmeister <ruzioe@gmail.com>
2012-09-25 17:40:32 -0500

    data added to data file

--- db/fixtures/school.rb
+++ db/fixtures/school.rb
@@ -4,5 +4,46 @@ School.seed do |s|
 	s.city = "Minneapolis"
 	s.state = "MN"
 	s.zip = "55412"
+  s.phone="612.668.2200"
 	s.ssw = "Edgar Young"
+end
+
+School.seed do |s|
+	s.name = "Nellie Stone Johnson"
+	s.street = "807 27th Ave N"
+	s.city = "Minneapolis"
+	s.state = "MN"
+	s.zip = "55411"
+  s.phone="612.668.2930"
+	s.ssw = "Teri Schmitz"
+end
+
+School.seed do |s|
+	s.name = "Andersen United"
+	s.street = "1098 Andersen Lane"
+	s.city = "Minneapolis"
+	s.state = "MN"
+	s.zip = "55407"
+  s.phone="612.668.4200"
+	s.ssw = "Julie Sailors"
+end
+
+School.seed do |s|
+	s.name = "Roosevelt High School"
+	s.street = "4029 28th Ave S"
+	s.city = "Minneapolis"
+	s.state = "MN"
+	s.zip = "55406"
+  s.phone="612.668.4800"
+	s.ssw = "Steve Carney"
+end
+
+School.seed do |s|
+	s.name = "Patrick Henry High School"
+	s.street = "4320 Newton Ave N"
+	s.city = "Minneapolis"
+	s.state = "MN"
+	s.zip = "55412"
+  s.phone="612.668.2000"
+	s.ssw = "Jenny Crouch"
 end
