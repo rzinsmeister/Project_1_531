3205e6b6c67abcd0ddd2a26d2b77aad900c0d520
Ruth Zinsmeister <ruzioe@gmail.com>
2012-09-27 15:45:20 -0500

    relationship added to the model

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ActiveRecord::Base
   attr_accessible :city, :name, :phone, :ssw, :state, :street, :zip
+  has_many :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
   attr_accessible :date_of_birth, :first_name, :grade, :last_name, :school_id
+  belongs_to :school
 end
