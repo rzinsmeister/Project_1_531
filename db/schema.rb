6194559f8082035508da972639721eedc223d344
Ruth Zinsmeister <ruzioe@gmail.com>
2012-11-03 17:19:06 -0500

    bcrypt added and a test user created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923025429) do
+ActiveRecord::Schema.define(:version => 20121103221222) do
 
   create_table "schools", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(:version => 20120923025429) do
     t.integer  "school_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
