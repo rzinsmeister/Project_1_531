bd82e6ddad649e37ac49db0f3649bbba46b57e58
Ruth Zinsmeister <ruzioe@gmail.com>
2012-11-18 20:01:45 -0600

    date view altered

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Education::Application.initialize!
+
+my_date_formats = { :default => '%m-%d-%Y' }
+Time::DATE_FORMATS.merge!(my_date_formats)
+Date::DATE_FORMATS.merge!(my_date_formats)
